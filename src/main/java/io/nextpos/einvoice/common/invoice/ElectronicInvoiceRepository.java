e02cef7c32cb325a479f59548405a51f6a47682a
joelin <tingjan1982@gmail.com>
2020-10-28 11:00:02 +0800

    Updated ElectronicInvoiceRepository.

--- src/main/java/io/nextpos/einvoice/common/invoice/ElectronicInvoiceRepository.java
+++ src/main/java/io/nextpos/einvoice/common/invoice/ElectronicInvoiceRepository.java
@@ -2,6 +2,9 @@ package io.nextpos.einvoice.common.invoice;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface ElectronicInvoiceRepository extends MongoRepository<ElectronicInvoice, String> {
 
+    Optional<ElectronicInvoice> findByInternalInvoiceNumber(String internalInvoiceNumber);
 }
